3da2c412f22c3ad6692a7004c1f5e6b43bd74561
George Papagapitos <george.g.papagapitos@gmail.com>
2021-08-23 10:27:09 -0500

    react router set up

--- spotify_controller/frontend/src/components/Home.js
+++ spotify_controller/frontend/src/components/Home.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import CreateRoom from './CreateRoom';
+import JoinRoom from './JoinRoom';
 
 export default function Home() {
   return (
-    <h1>Homepage</h1>
+    <Router>
+      <Switch>
+        <Route exact path='/'><p>This is the home page</p></Route>
+        <Route path='/join' component={JoinRoom} />
+        <Route path='/create' component={CreateRoom} />
+      </Switch>
+    </Router>
   )
 }
---
+++ spotify_controller/frontend/src/components/JoinRoom.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function JoinRoom() {
+  return (
+    <p>join room</p>
+  );
+}
