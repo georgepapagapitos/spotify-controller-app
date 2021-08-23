110e9a9b7a4dac28ef178445d7012272549dd5d2
George Papagapitos <george.g.papagapitos@gmail.com>
2021-08-23 15:45:02 -0500

    event handlers created and working

--- spotify_controller/frontend/src/components/JoinRoom.js
+++ spotify_controller/frontend/src/components/JoinRoom.js
@@ -5,20 +5,28 @@ import { TextField, Button, Grid, Typography } from '@material-ui/core';
 export default function JoinRoom() {
 
   const [roomCode, setRoomCode] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setRoomCode(e.target.value);
+  }
+
+  const handleClick = () => {
+    console.log('clicked');
+  }
 
   return (
     <Grid container spacing={1} align="center">
       <Grid item xs={12}>
         <Typography variant="h4" component="h4">
-          Join A Room
+          Join Room
         </Typography>
       </Grid>
       <Grid item xs={12}>
-        <TextField error={error} label="Room Code" placeholder="Enter a room code..." value={roomCode} helperText={error} variant="outlined" />
+        <TextField error={error} label="Room Code" placeholder="Enter a room code..." value={roomCode} helperText={error} variant="outlined" onChange={handleChange} />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" color="primary">Enter Room</Button>
+        <Button variant="contained" color="primary" onClick={handleClick}>Enter Room</Button>
       </Grid>
       <Grid item xs={12}>
         <Button variant="contained" color="secondary" to="/" component={Link}>Back</Button>
